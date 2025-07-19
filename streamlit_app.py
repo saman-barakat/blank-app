c29acdea54201c4879cf3491c20d6b6be9b8d191
Saman A. Barakat <saman.barakat@gmail.com>
2025-07-19 17:11:04 +0000

    Pages renamed

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.write(
 col1, col2 = st.columns(2)
 with col1:
     if st.button("Go to Page 1"):
-        st.switch_page("Pages/1_page1.py")
+        st.switch_page("pages/1_page1.py")
 with col2:
     if st.button("Go to Page 2"):
-        st.switch_page("Pages/2_page2.py")
+        st.switch_page("pages/2_page2.py")
