9caf73c6d995ff7dd8d5b433e77d1f338ca859de
Saman A. Barakat <saman.barakat@gmail.com>
2025-07-19 18:14:18 +0000

    Pages updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write(
 col1, col2 = st.columns(2)
 with col1:
     if st.button("Go to Info Page"):
-        st.switch_page("Info")
+        st.switch_page("pages/1_Info.py")
 with col2:
     if st.button("Go to Movies Page"):
-        st.switch_page("Movies")
+        st.switch_page("pages/2_Movies.py")
