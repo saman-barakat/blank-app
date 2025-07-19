68133636433bb5cc6fba5f52dc0f2b085d99a0cb
Saman A. Barakat <saman.barakat@gmail.com>
2025-07-19 17:16:55 +0000

    Files renamed

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.write(
 )
 col1, col2 = st.columns(2)
 with col1:
-    if st.button("Go to Page 1"):
-        st.switch_page("pages/1_page1.py")
+    if st.button("Go to Info Page"):
+        st.switch_page("Info")
 with col2:
-    if st.button("Go to Page 2"):
-        st.switch_page("pages/2_page2.py")
+    if st.button("Go to Movies Page"):
+        st.switch_page("Movies")
